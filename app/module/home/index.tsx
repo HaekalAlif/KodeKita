78d314839ce8eecb2ba503256856605bb3267c85
HaekalAlif <muhammad.haekal@mdi.id>
2025-11-04 10:50:19 +0700

    first commit

---
+++ app/module/home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeModule = () => {
+  return (
+    <div>
+      <h1>Welcome to KodeKita!</h1>
+      <p>Landing page is under development.</p>
+    </div>
+  );
+};
+
+export default HomeModule;
